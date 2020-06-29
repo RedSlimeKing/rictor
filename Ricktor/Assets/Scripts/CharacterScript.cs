141b98f113fa844901aad93b735e5478572e80b1
RedSlimeKing <DevonALy@outlook.com>
2020-06-28 23:49:36 -0400

    Third person Controls
    Create a basic third person controller
    - rotate camera with mouse
    - wasd movement
    - player turns where they are walking
    **Currents Bugs**
    Player snaps when turning

---
+++ Ricktor/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    private CharacterController cc;
+    public Transform cam;
+    private bool groundedPlayer;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+    private Vector3 playerVelocity;
+    public float playerSpeed = 5.0f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ------------------ Player Movement -----------------------------
+        groundedPlayer = cc.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            // Rotate player to look where he moving
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
+            cc.Move(moveDir.normalized * Time.deltaTime * playerSpeed);
+        }
+
+        // ----------------- Jump -----------------------------------------
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        cc.Move(playerVelocity * Time.deltaTime);
+
+        // ----------------- Camera movement ------------------------------
+        
+
+
+    }
+}
